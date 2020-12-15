69814943e0cd6b167a1c6e38ea5748aad7a63ea4
Loïc Stevens <stevensloic@gmail.com>
2020-12-15 23:15:54 +0100

    fix github url

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "heroku_build" "my_awesome_build" {
     app = "${heroku_app.my_awesome_app.id}"
     buildpacks = ["https://github.com/heroku/heroku-buildpack-python.git"]
     source = {
-        url = "https://github.com/heroku/python-getting-started"
+        url = "https://github.com/heroku/python-getting-started/archive/main.tar.gz"
         version = "main"
     }
 }
